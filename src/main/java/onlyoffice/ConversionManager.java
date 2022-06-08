724a497f9230c78aadab40818b6d050898411384
Andrey Yumatov <Andrey.yumatov@onlyoffice.com>
2022-06-08 19:29:10 +0300

    [Ref] Changing the sending of a conversion request

--- src/main/java/onlyoffice/ConversionManager.java
+++ src/main/java/onlyoffice/ConversionManager.java
@@ -1,16 +1,18 @@
 package onlyoffice;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.URL;
-import java.util.UUID;
-import java.nio.charset.StandardCharsets;
-import org.json.simple.parser.ParseException;
-import org.json.simple.parser.JSONParser;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.TrustStrategy;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.ContentType;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
@@ -23,9 +25,13 @@ import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
 
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServlet;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.net.URL;
+import java.util.UUID;
 
 @Named
 public class ConversionManager {
@@ -33,11 +39,10 @@ public class ConversionManager {
 
     @JiraImport
     private final PluginSettingsFactory pluginSettingsFactory;
-
     private final JwtManager jwtManager;
 
     @Inject
-    public ConversionManager(PluginSettingsFactory pluginSettingsFactory, JwtManager jwtManager) {
+    public ConversionManager(   PluginSettingsFactory pluginSettingsFactory, JwtManager jwtManager) {
                 
         this.pluginSettingsFactory = pluginSettingsFactory;
         this.jwtManager = jwtManager;
@@ -60,7 +65,7 @@ public class ConversionManager {
 
     public String GetConvertedUri(  String docUrl, String jwtSecret,
                                     String documentUri, String filename, String fromExtension, String toExtension, 
-                                    String filePass, Boolean isAsync, String lang, HttpServletResponse response) 
+                                    String filePass, Boolean isAsync, String lang) 
                                     throws Exception {
 
         String DocumentConverterUrl = docUrl + "ConvertService.ashx";
@@ -69,66 +74,57 @@ public class ConversionManager {
         title = title == null || title.isEmpty() ? UUID.randomUUID().toString() : title;
 
         String documentRevisionId = GenerateRevisionId(documentUri + title);
-        JSONObject body = new JSONObject();
-        body.put("region", lang);
-        body.put("url", documentUri);
-        body.put("outputtype", toExtension);
-        body.put("filetype", fromExtension);
-        body.put("title", title);
-        body.put("key", documentRevisionId);
-        body.put("password", filePass);
-        if (isAsync) body.put("async", true);
+        try (CloseableHttpClient httpClient = GetClient()) {
+            JSONObject body = new JSONObject();
+            body.put("region", lang);
+            body.put("url", documentUri);
+            body.put("outputtype", toExtension);
+            body.put("filetype", fromExtension);
+            body.put("title", title);
+            body.put("key", documentRevisionId);
+            body.put("password", filePass);
+            if (isAsync) body.put("async", true);
 
-        String headerToken = "";
-        if (jwtSecret != null && !jwtSecret.isEmpty()) {
-            JSONObject map = new JSONObject();
-            map.put("region", body.get("region"));
-            map.put("url", body.get("url"));
-            map.put("outputtype", body.get("outputtype"));
-            map.put("filetype", body.get("filetype"));
-            map.put("title", body.get("title"));
-            map.put("key", body.get("key"));
-            map.put("password", body.get("password"));
-            if (isAsync) map.put("async", body.get("async"));
+            StringEntity requestEntity = new StringEntity(body.toString(), ContentType.APPLICATION_JSON);
+                HttpPost request = new HttpPost(DocumentConverterUrl);
+                request.setEntity(requestEntity);
+                request.setHeader("Accept", "application/json");
 
-            String token = jwtManager.createToken(map);
-            body.put("token", token);
+            if (jwtManager.jwtEnabled()) {
+                String token = jwtManager.createToken(body);
+                JSONObject payloadBody = new JSONObject();
+                payloadBody.put("payload", body);
+                String headerToken = jwtManager.createToken(body);
+                body.put("token", token);
+                request.setHeader(jwtManager.getJwtHeader(), "Bearer " + headerToken);
+            }
 
-            JSONObject payloadMap = new JSONObject();
-            payloadMap.put("payload", map);
-            headerToken = jwtManager.createToken(payloadMap);
+            log.debug("Sending POST to Docserver: " + body.toString());
+            try(CloseableHttpResponse response = httpClient.execute(request)) {
+                int status = response.getStatusLine().getStatusCode();
+
+                if(status != HttpStatus.SC_OK) {
+                    throw new HttpException("Docserver returned code " + status);
+                } else {
+                    String content = IOUtils.toString(response.getEntity().getContent(), "utf-8");
+
+                    log.debug("Docserver returned: " + content);
+                    JSONObject callBackJSon = null;
+                    try{
+                        callBackJSon = new JSONObject(content);
+                    } catch (Exception e) {
+                        throw new Exception("Couldn't convert JSON from docserver: " + e.getMessage());
+                    }
+
+                    if (!callBackJSon.isNull("error") && callBackJSon.getInt("error") == -8) throw new SecurityException();
+                    
+                    if (callBackJSon.isNull("endConvert") || !callBackJSon.getBoolean("endConvert") || callBackJSon.isNull("fileUrl")) {
+                        throw new Exception("'endConvert' is false or 'fileUrl' is empty");
+                    }
+                    return callBackJSon.getString("fileUrl");
+                }
+            }
         }
-
-        byte[] bodyByte = body.toString().getBytes(StandardCharsets.UTF_8);
-
-        URL url = new URL(DocumentConverterUrl);
-        java.net.HttpURLConnection connection = (java.net.HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
-        connection.setFixedLengthStreamingMode(bodyByte.length);
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setConnectTimeout(ConvertTimeout);
-
-        if (jwtSecret != null && !jwtSecret.isEmpty())
-            connection.setRequestProperty(jwtManager.getJwtHeader(), "Bearer " + headerToken);
-        connection.connect();
-
-        try (OutputStream os = connection.getOutputStream()) {
-            os.write(bodyByte);
-            os.flush();
-        }
-        String jsonString = null;
-        try (InputStream stream = connection.getInputStream()) {
-            if (stream == null)
-                throw new Exception("Could not get an answer");
-            jsonString = ConvertStreamToString(stream);
-        } catch (Exception ex) {
-            response.setHeader("__ERROR__", ex.toString());
-        } finally {
-            connection.disconnect();
-        }
-        return GetResponseUri(jsonString);
     }
 
     public static String GenerateRevisionId(String expectedKey) {
@@ -140,51 +136,30 @@ public class ConversionManager {
         return key.substring(0, Math.min(key.length(), 20));
     }
 
-    private static String GetResponseUri(String jsonString) throws Exception {
-        JSONObject jsonObj = ConvertStringToJSON(jsonString);
+    private CloseableHttpClient GetClient() throws Exception {
+        CloseableHttpClient httpClient;
 
-        Object error = jsonObj.get("error");
-        if (error != null)
-            log.error(Math.toIntExact((long)error));
-
-        Boolean isEndConvert = (Boolean) jsonObj.get("endConvert");
-
-        Long resultPercent = 0l;
-        String responseUri = null;
-
-        if (isEndConvert) {
-            resultPercent = 100l;
-            responseUri = (String) jsonObj.get("fileUrl");
-        }
-        else {
-            resultPercent = (Long) jsonObj.get("percent");
-            resultPercent = resultPercent >= 100l ? 99l : resultPercent;
-        }
-
-        return resultPercent >= 100l ? responseUri : "";
-    }
-
-    public static String ConvertStreamToString(InputStream stream) throws IOException {
-        InputStreamReader inputStreamReader = new InputStreamReader(stream);
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String line = bufferedReader.readLine();
-
-        while (line != null) {
-            stringBuilder.append(line);
-            line = bufferedReader.readLine();
+        String cert = "false";
+        if (cert.equals("true")) {
+            log.debug("Ignoring SSL");
+            SSLContextBuilder builder = new SSLContextBuilder();
+            builder.loadTrustMaterial(null, new TrustStrategy() {
+                @Override
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            });
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(), new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            });
+            httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+        } else {
+            httpClient = HttpClients.createDefault();
         }
 
-        String result = stringBuilder.toString();
-
-        return result;
-    }
-
-    public static JSONObject ConvertStringToJSON(String jsonString) throws ParseException {
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(jsonString);
-        JSONObject jsonObj = (JSONObject) obj;
-
-        return jsonObj;
+        return httpClient;
     }
 }
